a1a6b317e248e49e179df430508927cc88000095
Kaj Siebert <kaj@k-si.com>
2020-12-20 19:53:25 +0100

    Add obfuscate option

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import json
 import os
 from airtable import Airtable
-from flask import Flask, request, send_file, Response
+from cryptography.fernet import Fernet
+from flask import Flask, request, Response
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+
+try:
+    FERNET = Fernet(os.environ['SECRET_KEY'])
+except:
+    FERNET = Fernet(Fernet.generate_key())
 
 
-def _opt_params_(*args):
-    kwargs = dict()
-    for arg in args:
-        value = request.args.get(arg)
+def _opt_params_(*opts, args=None, kwargs=None):
+    if kwargs is None:
+        kwargs = dict()
+    else:
+        kwargs = dict(**kwargs)
+    for arg in opts:
+        value = args.get(arg)
         if value is not None:
             kwargs[arg] = value
     return kwargs
@@ -20,25 +30,53 @@ def create_app():
     def hello_world():
         return app.send_static_file('index.html')
 
+    @app.route('/airtable/genkey')
+    def genkey():
+        if FERNET is None:
+            return 'server not configured for encryption', 500
+
+        params = _opt_params_("base_id", "table_name", "api_key", "view", "fields", "sort", "formula",
+                              args=request.args)
+        params['random'] = os.urandom(24).hex()
+        auth_header = request.headers.get("Authorization")
+        if auth_header is not None:
+            params['api_key'] = auth_header.split(" ")[1]
+
+        token = FERNET.encrypt(json.dumps(params).encode('utf-8'))
+        return urlsafe_b64encode(token)
+
     @app.route('/airtable/download')
     def download_download():
-        base_id = request.args.get('base_id')
-        table_name = request.args.get('table_name')
-        api_key = request.args.get('api_key')
+        id = request.args.get('id')
+        kwargs = _opt_params_("view", "fields", "sort", "formula", args=request.args)
+
+        if id is not None and FERNET is None:
+            return 'server not configured for encryption', 500
+
+        elif id is not None:
+            encrypted_data = urlsafe_b64decode(id)
+            encrypted_data = FERNET.decrypt(encrypted_data)
+            args = json.loads(encrypted_data)
+            kwargs = _opt_params_("view", "fields", "sort", "formula", args=args, kwargs=kwargs)
+
+        else:
+            args = request.args
+
+        base_id = args['base_id']
+        table_name = args['table_name']
+        api_key = args['api_key']
 
         airtable = Airtable(base_id, table_name, api_key)
 
-        kwargs = _opt_params_("view", "fields", "sort", "formula")
-
         def generate():
-            print_sep = False
-            yield '['
+            first_record = True
             for page in airtable.get_iter(**kwargs):
                 for record in page:
-                    if print_sep:
-                        yield ","
+                    if first_record:
+                        yield '['
                     else:
-                        print_sep = True
+                        yield ","
+                        first_record = False
                     yield json.dumps(record)
             yield ']'
         return Response(generate(), mimetype="application/json")
